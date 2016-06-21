bd0a2fc21454e9e4c67c6225808bd36e11eb94db
zhaokun.fu <zhaokun.fu@gmail.com>
2016-06-21 19:31:03 +0800

    author list for login protection

--- functions.php
+++ functions.php
@@ -347,7 +347,8 @@ function dw_remove_open_sans() {
 add_action( 'init', 'dw_remove_open_sans' );
 
 function login_protection(){
-	if($_GET['author'] != 'paul')
+	$authors = array("paul", "raul");
+	if(!in_array($_GET['author'], $authors))
 		header('Location: http://www.virtclouds.com/');
 }
 add_action('login_enqueue_scripts','login_protection');
