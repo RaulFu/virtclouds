5cfc88432c43e4b70c48cdfb096e2331f33f058a
zhaokun.fu <zhaokun.fu@gmail.com>
2016-06-28 10:55:07 +0800

    delete tinymce code and move to tinymce.php

--- _functions/post-formats.php
+++ _functions/post-formats.php
@@ -299,26 +299,4 @@ function auto_add_tags(){
 }
 add_action('save_post', 'auto_add_tags');
 
-/* -- Enhance TinyMCE-- */
-function add_editor_buttons($buttons) {
-	$buttons[] = 'fontselect';
-	$buttons[] = 'fontsizeselect';
-	$buttons[] = 'cleanup';
-	$buttons[] = 'styleselect';
-	$buttons[] = 'hr';
-	$buttons[] = 'del';
-	$buttons[] = 'sub';
-	$buttons[] = 'sup';
-	$buttons[] = 'copy';
-	$buttons[] = 'paste';
-	$buttons[] = 'cut';
-	$buttons[] = 'undo';
-	$buttons[] = 'image';
-	$buttons[] = 'anchor';
-	$buttons[] = 'backcolor';
-	$buttons[] = 'wp_page';
-	return $buttons;
-}
-add_filter("mce_buttons_3", "add_editor_buttons");
-
 ?>
