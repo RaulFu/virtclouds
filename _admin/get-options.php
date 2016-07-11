70d663e3fe6b111b1e02a4d22f98d9320a4a8df5
zhaokun.fu <zhaokun.fu@gmail.com>
2016-07-11 15:08:42 +0800

    add array check before foreach to fix warning

--- _admin/get-options.php
+++ _admin/get-options.php
@@ -1,15 +1,19 @@
 <?php
 global $options;
 
-foreach ($options as $value) {
-	
-	if(!isset($value['id'])) continue;
-    
-    if (get_option( $value['id'] ) === FALSE) {
-    	$$value['id'] = (isset($value['std']) ? $value['std'] : null);
-    } else { 
-    	$$value['id'] = get_option( $value['id'] );
-    }
+if(is_array($options))
+{
+	foreach ($options as $value) {
+		
+		if(!isset($value['id'])) continue;
+	    
+	    if (get_option( $value['id'] ) === FALSE) {
+		$$value['id'] = (isset($value['std']) ? $value['std'] : null);
+	    } else { 
+		$$value['id'] = get_option( $value['id'] );
+	    }
 
+	}
 }
 ?>
+
