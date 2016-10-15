be95a8279b178280f814a5f4580012377c32943e
zhaokun.fu <zhaokun.fu@gmail.com>
2016-10-15 06:49:33 +0000

    drop auto add tags

--- _functions/post-formats.php
+++ _functions/post-formats.php
@@ -282,21 +282,4 @@ function aw_meta_save( $post_id ) {
 }
 add_action( 'save_post', 'aw_meta_save' );
 
-/**
- * WordPress 自动为文章添加已使用过的标签
- */
-function auto_add_tags(){
-	$tags = get_tags( array('hide_empty' => false) );
-	$post_id = get_the_ID();
-	$post_content = get_post($post_id)->post_content;
-	if ($tags) {
-		foreach ( $tags as $tag ) {
-			// 如果文章内容出现了已使用过的标签，自动添加这些标签
-			if ( strpos($post_content, $tag->name) !== false)
-				wp_set_post_tags( $post_id, $tag->name, true );
-		}
-	}
-}
-add_action('save_post', 'auto_add_tags');
-
 ?>
